f4c7959ab3f8906f630c3cc59430ebaaf5c778e4
SerhiiValchuk <valthuk@yahoo.com>
2021-07-19 20:04:24 +0300

    home2.3

--- lesson1/index.js
+++ lesson1/index.js
@@ -51,4 +51,30 @@ function hhh(q, r) {
     return Math.sqrt(q ** 2 + r ** 2)
 }
 let result = hhh(n, m);
-console.log(result)
+console.log(result);
+
+
+/*Вывести в окно браузера при помощи метода alert() следующие данные: Ваше ФИО, возраст, хобби (каждой на новой строки спомощью \n).*/
+alert('Serhii Valchuk, \n 29,\n sport');
+
+/*Создать 4 переменные с использованием ключевого слова let с именами str1, str2, str3, concatenation.
+  Переменной str1 присвоить фразу ‘Кто ‘, str2 – ‘ты ‘, str3 – ‘такой?’
+  Локальной переменной concatenation присвоить результат конкатенации 3-х строк: str1, str2, str3.
+  Вывести в документ содержимое переменной concatenation спомощью document.write
+  */
+let str1 = 'Кто ';
+let str2 = 'ты ';
+let str3 = 'такой';
+let concatenation = (str1 + str2 +str3);
+// document.write(concatenation);
+/*Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?*/
+let str4 = "20";
+let a = 5;
+document.write(str4 + a + "<br/>");205
+document.write(str4 - a + "<br/>");15
+document.write(str4 * "2" + "<br/>");40
+document.write(str4 / 2 + "<br/>");10
+
+
+
+
