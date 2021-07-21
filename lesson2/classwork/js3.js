c9e72e030152cc4c3f6cf41300d935f0e0eff650
SerhiiValchuk <valthuk@yahoo.com>
2021-07-21 20:04:41 +0300

    home

--- lesson2/classwork/js3.js
+++ lesson2/classwork/js3.js
@@ -22,6 +22,39 @@ for (let i = 0; i<10;i++){
     document.write(`<div>Lorem ipsum.</div>`);
 }
 //- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
-for (let i = 0; i<10;i++) {
-    document.write(`<div>Lorem ipsum.+ indexOf</div>`);
+for (let i = 0; i<10.;i++) {
+    document.write(`<div>${i} :Lorem ipsum.</div>`);
 }
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+let i = 0;
+while(i<20) {
+    document.write(`<h1>Lorem ipsum dolor sit amet.</h1>`);
+    i++;
+}
+//- За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+let ii = 0;
+while(ii<20) {
+    document.write(`<h1>${ii} :Lorem ipsum dolor sit amet.</h1>`);
+    ii++;
+}
+//- Створити масив з 10 числових елементів. Вивести в консоль всі його елементи в циклі.
+let mas= [23,45,67,4,6,7,6,7,4,9];
+  for (let i=0;i<mas.length;i++){
+    console.log(mas[i]);
+}
+
+ //- Створити масив з 10 строкрових елементів. Вивести в консоль всі його елементи в циклі.
+let text= ['sea','ocean','road','lace','mountain','river','hill','food','bag','drag'];
+  for (let i=0;i<text.length;i++){
+    console.log(text[i]);
+}
+  //- Створити масив з 10 елементів будь-якого типу. Вивести в консоль всі його елементи в циклі.
+let others = [1,'sea','ocean',null,undefined,45,78,true,'mountain','river'];
+for (let i=0;i<others.length;i++){
+    console.log(others[i]);
+}
+//Створити масив з 10 елементів числового, стірчкового і булевого типу. За допомогою if та typeof вивести тільки булеві елементи
+let all = [1,'sea','ocean',null,undefined,45,78,true,'mountain','river'];
+ for(const al of all){
+
+ }
