2b53f28d7e4cb87ca891c69da9a5fe123e66d01c
SerhiiValchuk <valthuk@yahoo.com>
2021-07-21 14:08:13 +0300

    home

--- lesson3/homework/index3.js
+++ lesson3/homework/index3.js
@@ -19,6 +19,44 @@ let S3 = 's = '+ area3(3,2);
 console.log(S3);
 //- створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);//
 
-function numbers() {
-
+function max_min() {
+ let max = arguments[0];
+ let min = arguments[0];
+ for (i = 0; i < arguments.length; i++) {
+  if (arguments[i] > max)
+  {
+   max = arguments[i];
+  }
+  if (arguments[i] < min)
+  {
+   min = arguments[i];
+  }
+ }
+ console.log(max);
+ return min;
 }
+console.log(max_min(2,56,76,5,4));
+// - створити функцію яка  створює блок з текстом. Текст задати через аргумент
+function texts(text){
+  document.write(`<p>${text}</p>`);
+}
+texts('some_text');
+// - створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+function ul(li){
+ document.write(`<ul>
+<li>${li}</li>
+<li>${li}</li>
+<li>${li}</li>
+</ul>`);
+}
+ul('Lorem ipsum dolor sit amet.');
+//- створити функцію яка  створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)//
+function ul2(li){
+ for (let i
+
+ document.write(`<ul>
+<li>${li}</li>
+</ul>`);
+}
+ul2('Lorem ipsum dolor sit amet.');
