1d75d161634ae5e0948aaff8e089073d8711ad3e
SerhiiValchuk <valthuk@yahoo.com>
2021-07-21 18:27:35 +0300

    home

--- lesson2/classwork/js3.js
+++ lesson2/classwork/js3.js
@@ -18,3 +18,10 @@ massiw[3] = 'go';
 massiw[5] = 45;
 console.log(massiw);
 // За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+for (let i = 0; i<10;i++){
+    document.write(`<div>Lorem ipsum.</div>`);
+}
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+for (let i = 0; i<10;i++) {
+    document.write(`<div>Lorem ipsum.+ indexOf</div>`);
+}
